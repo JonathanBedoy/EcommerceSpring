aedd8efd47eca20d81a59e13e613bd9155f2a16e
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2023-02-22 19:49:08 -0800

    added dockerfile and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO item (id, quantity, price, description, name) VALUES
+(
+    1,
+    4,
+    36.79,
+    'Black Hat for summer',
+    'Hat'
+),
+(
+    2,
+    15,
+    3.99,
+    'Black T shirt',
+    'Shirt'
+);
